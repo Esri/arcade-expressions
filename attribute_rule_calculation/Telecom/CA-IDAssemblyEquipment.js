5e6f2e12a736c5126de26457b3db6a806c043674
Michael Miller <mmiller@esri.com>
2020-04-22 16:54:44 -0400

    new rules for IDs of equipment

--- attribute_rule_calculation/Telecom/CA-IDAssemblyEquipment.js
+++ attribute_rule_calculation/Telecom/CA-IDAssemblyEquipment.js
@@ -1,7 +1,7 @@
-// Assigned To: CommunicationsDevice
-// Name: Calc Port ID by Card
-// Description: Calculate the port ID by the content of a card
-// Subtypes: Port
+// Assigned To: CommunicationsAssembly
+// Name: Calculate Assembly Equipment ID
+// Description:  Calculate Assembly Equipment ID by parent container
+// Subtypes: Assembly Equipment
 // Field: AssetID
 // Execute: Insert, Update
 
@@ -9,32 +9,38 @@
 // This section has the functions and variables that need to be adjusted based on your implementation
 var assigned_to_field = $feature.assetid;
 if (IsEmpty(assigned_to_field) == false && assigned_to_field != '') {
-    assigned_to_field
+    return assigned_to_field
 }
 
-var container_class = 'CommunicationsAssembly';
-var device_class = 'CommunicationsDevice';
 // Limit the rule to valid subtypes
-var valid_asset_groups = [8];
-var valid_asset_types = [145];
+var valid_asset_groups = [4];
+var valid_asset_types = [121, 122, 123, 124, 125, 127];
+
 
 if (count(valid_asset_groups) > 0 && indexof(valid_asset_groups, $feature.assetgroup) == -1) {
-    assigned_to_field;
+    return assigned_to_field;
 }
 if (count(valid_asset_types) > 0 && indexof(valid_asset_types, $feature.assettype) == -1) {
-    assigned_to_field;
+    return assigned_to_field;
+}
+
+var container_class;
+var self_class = 'CommunicationsAssembly';
+if ($feature.assettype == 123) {
+    container_class = 'StructureBoundary';
+} else {
+    container_class = 'CommunicationsAssembly';
 }
 var id_split = ':';
 var at_to_id_index = {
-    '121': 1,
+    '123': 1,
     '122': 2,
-    '123': 3,
+    '127': 3,
     '124': 4,
     '125': 5,
-    '127': 6,
+    '121': 6,
     '145': -1
 };
-var equipment_id_len = 7;
 
 // Get Feature Switch yard, adjust the string literals to match your GDB feature class names
 function get_features_switch_yard(class_name, fields, include_geometry) {
@@ -42,8 +48,8 @@ function get_features_switch_yard(class_name, fields, include_geometry) {
     var feature_set = null;
     if (class_name == "CommunicationsDevice") {
         feature_set = FeatureSetByName($datastore, "CommunicationsDevice", fields, include_geometry);
-    } else if (class_name == "CommunicationsLine") {
-        feature_set = FeatureSetByName($datastore, "CommunicationsLine", fields, include_geometry);
+    } else if (class_name == "StructureBoundary") {
+        feature_set = FeatureSetByName($datastore, "StructureBoundary", fields, include_geometry);
     } else if (class_name == "CommunicationsAssembly") {
         feature_set = FeatureSetByName($datastore, "CommunicationsAssembly", fields, include_geometry);
     } else if (class_name == 'Associations') {
@@ -88,44 +94,57 @@ function has_bit(num, test_value) {
 
 var association_status = $feature.ASSOCIATIONSTATUS;
 if (IsEmpty(association_status) || (has_bit(association_status, 4) || has_bit(association_status, 16)) == false) {
-    return assigned_to_field
+    return assigned_to_field;
 }
 
 var container_row = First(FeatureSetByAssociation($feature, 'container'));
+
 var global_id = container_row['globalid'];
-var feature_set = get_features_switch_yard(container_class, ['Globalid', 'assetid'], false);
-var container_feature = First(Filter(feature_set, "globalid = @global_id"));
+var container_feature_set = get_features_switch_yard(container_class, ['globalid', 'assetid'], false);
+
+var container_feature = First(Filter(container_feature_set, "globalid = @global_id"));
+if (IsEmpty(container_feature)) {
+    return {'errorMessage': 'Container could not be found'};
+}
 var content_rows = FeatureSetByAssociation(container_feature, 'content');
+
 var global_ids = [];
 var i = 0;
 for (var content_row in content_rows) {
+    if (content_row.globalid == $feature.globalid) {
+        continue;
+    }
     global_ids[i++] = content_row.globalid;
 }
-var other_port_sql = 'AssetGroup = ' + $feature.assetgroup + ' AND  AssetType = ' + $feature.assettype;
+var other_equipment_sql = 'AssetGroup = ' + $feature.assetgroup + ' AND AssetType = ' + $feature.assettype;
 var at = Text($feature.assettype);
 if (HasKey(at_to_id_index, at) == false) {
     return {'errorMessage': 'Asset Type:' + at + ' missing from ID format in Attribute Rule'};
 }
 
 var split_idx = at_to_id_index[at];
-
-var feature_set = get_features_switch_yard(device_class, ['Globalid', 'AssetID', 'AssetGroup', 'AssetType'], false);
-var features = Filter(feature_set, "globalid IN @global_ids and " + other_port_sql);
-
-var existing_ports = [];
-for (var feature in features) {
-    var feat_id = feature.assetid;
-    return feat_id
-    feat_id = Split(feat_id, id_split);
-    return feat_id
-    if (Count(feat_id == 7)) {
-        existing_ports[Count(existing_ports)] = feat_id[split_idx];
-    }
-}
 var next_id = 1;
-if (Count(existing_ports) > 0) {
-    next_id = sort(existing_ports)[-1];
-    next_id += 1;
+if (count(global_ids) > 0) {
+
+    var content_feature_set = get_features_switch_yard(self_class, ['Globalid', 'AssetID', 'AssetGroup', 'AssetType'], false);
+    var features = Filter(content_feature_set, "globalid IN @global_ids and " + other_equipment_sql);
+    var existing_ports = [];
+    for (var feature in features) {
+        var feat_id = feature['assetid'];
+        if (IsEmpty(feat_id) || feat_id == '') {
+            continue
+        }
+        var feat_id_split = Split(feat_id, id_split);
+
+        if (Count(feat_id_split) > split_idx) {
+            existing_ports[Count(existing_ports)] = Number(feat_id_split[split_idx]);
+        }
+    }
+
+    if (Count(existing_ports) > 0) {
+        next_id = Number(sort(existing_ports)[-1]);
+        next_id += 1;
+    }
 }
 var parent_id_arr = Split(container_feature.assetid, id_split);
 parent_id_arr[split_idx] = next_id;
